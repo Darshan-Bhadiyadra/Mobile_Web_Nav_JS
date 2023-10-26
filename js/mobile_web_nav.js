c2d318a52d3be7b1b15705623d63b053efb73295
Darshan Bhadiyadra <129250327+Darshan-Bhadiyadra@users.noreply.github.com>
2023-10-26 10:31:33 +0530

    Add files via upload

---
+++ js/mobile_web_nav.js
@@ -0,0 +1,19 @@
+// function updateImgPath($img) {
+//     document.getElementById("image").src = "image/" + $img + ".webp";
+// }
+
+function function1() {
+    document.getElementById("image").src = "image/h1.png";
+}
+
+function function2() {
+    document.getElementById("image").src = "image/w.jpg";
+}
+
+function function3() {
+    document.getElementById("image").src = "image/b.webp";
+}
+
+function function4() {
+    document.getElementById("image").src = "image/a.webp";
+}
